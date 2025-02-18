91448933e2111e066b9240e5e2de438d3cfaceb0
codhu24 <ahmednizam0024@gmail.com>
2025-02-18 21:44:08 +0530

    initial release

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('openGmailBtn').addEventListener('click', function() {
+    const email = 'ahmednizam0024@gmail.com';
+    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email}`, '_blank');
+});
